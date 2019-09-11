fe1ebe25829d55cea7dc4049ebeb8587f10c761f
Laura Pérez <37106023+lauspace@users.noreply.github.com>
2019-09-11 21:15:03 +0200

    Create Exemple_2.c

---
+++ Exemple_2.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main ( ) {
+	int operador_1;
+	int operador_2;
+	int resultat;
+	operador_1 = 200;
+	operador_2 = 300;
+	resultat = operador_1 + operador_2;
+  printf ("Resultat = %d", resultat);
+}
