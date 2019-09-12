fa6b12505205520702d56c100c1fc441efae3f7a
Laura Pérez <37106023+lauspace@users.noreply.github.com>
2019-09-12 19:21:33 +0200

    Create Exemple_06.py

---
+++ Exemple_06.py
@@ -0,0 +1,6 @@
+xifra_usuari = 0
+numero_secret = 15
+while xifra_usuari != numero_secret:
+  print ("Endevina el número secret entre l'1 i el 20: ")
+  xifra_usuari = int(input())
+print ("Has endevinat el número!")
